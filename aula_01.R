b3241a1236000371345021586bbf696be1b159eb
Sillas <sillas.gonzaga@gmail.com>
2021-10-11 18:49:09 -0300

    aula 01

---
+++ aula_01.R
@@ -0,0 +1,56 @@
+4 + 11
+
+6 - 15
+6 ^ 3
+
+#usando o r para operações mais complexas
+(4 + 9) / 3   # meu comentario
+
+x = 4
+
+x2 = 10
+
+y <- -9
+
+z <- 15
+
+z * y
+x - y
+
+# funções
+sqrt(x)
+
+sqrt(y)
+
+
+sqrt(z)
+
+round(sqrt(z), 1)
+round(sqrt(z), 2)
+round(sqrt(z))
+
+
+round(4.12345678, 4)
+round(digits = 4, x = 11.12345678)
+
+help(round)
+?round
+
+library(readr)
+
+install.packages("readr")
+install.packages("tidyverse")
+
+library(tidyverse)
+
+read.csv2()
+
+
+df <- readr::read_csv2("Ano-2021.csv")
+
+
+
+
+
+
+
